f292fec84eb329589c080d47b08ddc347d64512c
tatsuro osuka <oscarwild13@gmail.com>
2020-05-17 19:30:50 +0900

    product module add 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductModule } from './product/product.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '', component: ProductListComponent},
+  // {path: 'detail', component: ProductDetailComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  ProductModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from "./common/navbar/navbar.component";
-import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    ProductListComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
