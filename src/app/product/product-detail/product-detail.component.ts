e3995f2da1bda057145042ec409cc557cbfa3e87
tatsuro <oscarwild13@gmail.com>
2020-05-28 16:27:38 +0900

    フロントとDBをつなぐ

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from '../../product'
-
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent implements OnInit {
-  product
+  product;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private productservise: ProductService
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')]
-    })
+    this.route.paramMap.subscribe((params) => {
+      const productObservable = this.productservise.getProductId(
+        params.get('productId')
+      );
+      productObservable.subscribe(
+        (data) => {
+          this.product = data;
+        },
+        (err) => {}
+      );
+    });
   }
-
 }
--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from '../../product'
-
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-listings',
   templateUrl: './product-listings.component.html',
-  styleUrls: ['./product-listings.component.scss']
+  styleUrls: ['./product-listings.component.scss'],
 })
 export class ProductListComponent implements OnInit {
+  products: any;
 
-  products: any
-
-  constructor() { }
+  constructor(private productservice: ProductService) {}
 
   ngOnInit(): void {
-    this.products = products
-  }
+    // this.products = this.productservice.getProducts();
 
+    const productObservable = this.productservice.getProducts();
+    productObservable.subscribe(
+      (data) => {
+        this.products = data;
+        //console.log('got value ' + data);
+      },
+      (err) => {
+        console.error('something wrong occurred: ' + err);
+      }
+    );
+  }
 }
