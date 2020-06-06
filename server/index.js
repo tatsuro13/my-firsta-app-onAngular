b79febc0d88e383b7996b22d42590e300753a94f
tatsuro <oscarwild13@gmail.com>
2020-06-06 18:21:52 +0900

    バックエンド側パスワード周り

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const config = require("./config");
 const FakeDb = require("./fake-db");
 
 const productRoutes = require("./routes/products");
+const userRoutes = require("./routes/users");
 const path = require("path");
 
 mongoose
   .connect(config.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => {
     if (process.env.NODE_ENV !== "production") {
@@ -20,7 +23,10 @@ mongoose
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const appPath = path.join(__dirname, "..", "dist", "my-first-app");
